e550c90c53ac95af685cd037c430c954263da6b7
Radek Molenda <radek.molenda@onthebeach.co.uk>
2014-01-07 12:56:57 +0000

    Pass data to Renderer

--- build/app.js
+++ build/app.js
@@ -1,23 +1,31 @@
 /** @jsx React.DOM */
 
 var App = React.createClass({
+  handleEditorChange: function (data) {
+    console.log(data);
+  },
   render: function () {
     return (
       <div className="editors">
-        <Editor />
-        <Renderer />
+        <Editor onEditorChange={this.handleEditorChange}/>
+        <Renderer data="Some fancy stuff here"/>
       </div>
     );
   }
 });
 
 var Editor = React.createClass({
+  handleChange: function () {
+    this.props.onEditorChange(this.refs.editor.getDOMNode().value);
+  },
   render: function () {
     return (
       <div className="editor large-6 columns">
         <textarea
+          ref="editor"
           className="editor large-6.columns panel"
-          placeholder="Write some **Markdown** here" />
+          placeholder="Write some **Markdown** here"
+          onChange={this.handleChange} />
       </div>
     );
   }
@@ -26,7 +34,7 @@ var Editor = React.createClass({
 var Renderer = React.createClass({
   render: function () {
     return (
-      <div className="renderer large-6 columns panel"></div>
+      <div className="renderer large-6 columns panel" >{this.props.data}</div>
     );
   }
 });
